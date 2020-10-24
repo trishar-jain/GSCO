e0db1d30424154610c0e0a63e09e02253266723e
trishar-jain <71705284+trishar-jain@users.noreply.github.com>
2020-10-24 21:21:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var car,car2,car3,car,4
+var rod1,rod2,rod3,rod4
+
+
+function setup() {
+  createCanvas(1600,400);
+  rod1 = createSprite(400, 100, 10000, 10);
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+}
